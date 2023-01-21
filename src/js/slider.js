a48a1aafbb18471645e4c21155e331c0e579c8e5
andrsweb <proflinervk@gmail.com>
2023-01-22 00:59:02 +0300

    create press section

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,12 +3,13 @@ import Swiper, { Pagination, Navigation } from "swiper"
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
 
-	initSwiper('.before-after-swiper', 2, '.before-after-next', '.before-after-prev', 2)
-	initSwiper('.rhino-swiper', 2, '.white-next', '.white-prev', 1 )
-	initSwiper('.media-swiper', 1, '.media-next', '.media-prev', 1 )
+	initSwiper('.before-after-swiper', 2, '.before-after-next', '.before-after-prev', 2 , '.swiper-pagination' )
+	initSwiper('.rhino-swiper', 2, '.white-next', '.white-prev', 1, '.swiper-pagination' )
+	initSwiper('.media-swiper', 1, '.media-next', '.media-prev', 1, '.media-pagination' )
+	initSwiper('.press-swiper', 4, '.swiper-button-next', 'swiper-button-prev', 4, '.press-pagination' )
 })
 
-const initSwiper = (slider, num, next, prev, slides ) => {
+const initSwiper = (slider, num, next, prev, slides, pag ) => {
 
 	const swiper = new Swiper(slider, {
 
@@ -28,7 +29,7 @@ const initSwiper = (slider, num, next, prev, slides ) => {
 		modules: [Pagination, Navigation],
 
 			pagination: {
-				el: '.swiper-pagination',
+				el: pag,
 				clickable: true
 			},
 

7c62e61bf46ef64251cc5d11334ad40f17f9702f
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-01-22 20:17:59 +0300

    feat: add toggle to Study list.

--- src/js/about/showMore.js
+++ src/js/about/showMore.js
@@ -1,4 +1,52 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 	'use strict'
+
+	toggleShowMore()
 } )
 
+const toggleShowMore = () => {
+	const button	= document.querySelector( '.study-button' ),
+		list		= document.querySelector( '.study-more-items' )
+
+	if( ! button || ! list ) return
+
+	closeList( list )
+
+	button.addEventListener( 'click', () => {
+		if( list.classList.contains( 'opened' ) ) closeList( list )
+		else openList( list )
+	} )
+}
+
+const outerHeight = el => {
+	const style	= getComputedStyle( el )
+	let height	= el.offsetHeight
+
+	return height + parseInt( style.marginTop ) + parseInt( style.marginBottom )
+}
+
+const openList = list => {
+	const hiddenItems	= list.querySelectorAll( '.to-hide' )
+	let listHeight		= parseInt( getComputedStyle( list ).height )
+
+	if( ! hiddenItems.length ) return
+
+	hiddenItems.forEach( item => listHeight += outerHeight( item ) )
+	list.classList.add( 'opened' )
+	list.style.height = `${ listHeight }px`
+}
+
+const closeList = list => {
+	const alwaysVisibleItems = list.querySelectorAll( '.always-visible' )
+	let listInitialHeight = 0
+
+	if( ! alwaysVisibleItems.length ) return
+
+	alwaysVisibleItems.forEach( item => listInitialHeight += outerHeight( item ) )
+	list.style.height = `${ listInitialHeight }px`
+	list.classList.remove( 'opened' )
+}
+
+window.addEventListener( 'resize', () => {
+	closeList( document.querySelector( '.study-more-items' ) )
+} )

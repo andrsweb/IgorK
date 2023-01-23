bd457e9af89d0c2a588c623c820f72460885a2e4
andrsweb <proflinervk@gmail.com>
2023-01-23 20:35:39 +0300

    add some videos

--- src/js/lazy-frame.js
+++ src/js/lazy-frame.js
@@ -3,11 +3,31 @@ import lazyframe from "lazyframe"
 document.addEventListener( 'DOMContentLoaded', () => {
 	'use strict'
 
-	let elements = document.querySelectorAll( '.video-frame' )
+	lazy()
+	removePlayButton()
+} )
+
+const lazy = () => {
+	let elements = document.querySelectorAll( '.frame' )
 
 	lazyframe(elements, {
 		debounce: 1000,
 		lazyload: true,
 		autoplay: true
 	})
-} )
+}
+
+const removePlayButton = () => {
+	const wrapper = document.querySelectorAll( '.frame-wrapper')
+
+	if( ! wrapper ) return
+
+	wrapper.forEach( item => {
+
+		item.addEventListener( 'click', () => {
+			if( ! item.classList.contains( 'played' ) ) {
+				item.classList.add( 'played' )
+			}
+		} )
+	} )
+}

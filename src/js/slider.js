a2029b771eb840296efcb49747430082158d3060
andrsweb <proflinervk@gmail.com>
2023-01-29 20:29:50 +0300

    more fixes

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,7 +3,10 @@ import Swiper, { Pagination, Navigation } from "swiper"
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
 
-	initSwiper('.before-after-swiper', 2, '.before-after-next', '.before-after-prev', 2 , '.swiper-pagination' )
+	initSwiper('.face-swiper', 2, '.before-after-next', '.before-after-prev', 2 , '.face-pagination' )
+	initSwiper('.bleph-swiper', 2, '.before-after-next', '.before-after-prev', 2 , '.blef-pagination' )
+	initSwiper('.waist-swiper', 2, '.before-after-next', '.before-after-prev', 2 , '.waist-pagination' )
+	initSwiper('.bust-swiper', 2, '.before-after-next', '.before-after-prev', 2 , '.bust-pagination' )
 	initSwiper('.rhino-swiper', 2, '.white-next', '.white-prev', 1, '.swiper-pagination' )
 	initSwiper('.media-swiper', 1, '.media-next', '.media-prev', 1, '.media-pagination' )
 	initSwiper('.press-swiper', 4, '.press-next', 'press-prev', 4, '.press-pagination' )
@@ -11,8 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	initSwiper('.work-face-swiper', 2, '.before-after-next', '.before-after-prev', 2 , '.work-face-pagination' )
 	initSwiper('.blepharo-swiper', 2, '.blepharo-next', 'blepharo-prev', 2, '.blef-pagination' )
 	initSwiper('.work-rhino-swiper', 2, '.work-rhino-next', 'work-rhino-prev', 2, '.rhino-pagination1' )
-	initSwiper('.work-waist-swiper', 2, '.work-waist-next', 'work-waist-prev', 2, '.swiper-pagination' )
-	initSwiper('.work-bust-swiper', 2, '.work-bust-next', 'work-bust-prev', 2, '.swiper-pagination' )
+	initSwiper('.work-waist-swiper', 2, '.work-waist-next', 'work-waist-prev', 2, '.waist-pagination' )
+	initSwiper('.work-bust-swiper', 2, '.work-bust-next', 'work-bust-prev', 2, '.bust-pagination' )
 	initSwiper('.tv-swiper', 1, '.tv-next', '.tv-prev', 1, '.swiper-pagination' )
 })
 
